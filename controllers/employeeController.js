ff342f2ba24c45afed4d70b3f332f667b5d41783
nizhantiw <nizhantiw@gmail.com>
2023-04-29 20:40:40 +0530

    Update employeeController.js

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,43 +1,90 @@
 const Employee = require('../models/employeeModel');
 
-//Registering Employee into database
+const getPaginatedEmployees = async (req, res) => {
+  try {
+    const sort = { salary: 1 };
+    const limit = parseInt(req.query.limit) || 5;
+    const page = parseInt(req.query.page) || 1;
+    const skip = (page - 1) * limit;
+
+    const employees = await Employee.find().sort(sort).skip(skip).limit(limit);
+    const count = await Employee.countDocuments();
+
+    res.status(200).json({ employees, count });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const deleteHighSalaryEmployees = async (req, res) => {
+  try {
+    const result = await Employee.deleteMany({ salary: { $gt: 10000 } });
+    if (result.deletedCount === 0) {
+      res.status(404).json({ error: 'No employees found to delete' });
+    } else {
+      res.status(200).json({ message: 'Employees deleted successfully' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 const createEmployee = async (req, res) => {
   try {
-    // Write a code here to store Employee data
+    const { firstName, lastName, email, salary } = req.body;
+    if (!firstName || !lastName || !email || !salary) {
+      res.status(400).json({ error: 'Missing required fields' });
+    } else {
+      const employee = new Employee({ firstName, lastName, email, salary });
+      await employee.save();
+      res.status(201).json({ message: 'Employee created successfully', employee });
+    }
   } catch (err) {
-    res.status(500).json({ error: 'Failed to create employee' });
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-//Get Employee From a Particular id
-const getEmployee = async (req, res) => {
+const getEmployeeById = async (req, res) => {
   try {
-    // Write a code here to get Employee from a Particular id
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+      res.status(404).json({ error: 'Employee not found' });
+    } else {
+      res.status(200).json({ employee });
+    }
   } catch (err) {
-    res.status(500).json({ error: 'Failed to get employee details' });
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-//Updating Employee
-const updateEmployee = async (req, res) => {
+const updateEmployeeById = async (req, res) => {
   try {
-    //Write a code here for updating Employee details using 'PUT' request
+    const { firstName, lastName, email, salary } = req.body;
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+      res.status(404).json({ error: 'Employee not found' });
+    } else {
+      employee.firstName = firstName || employee.firstName;
+      employee.lastName = lastName || employee.lastName;
+      employee.email = email || employee.email;
+      employee.salary = salary || employee.salary;
+      await employee.save();
+      res.status(200).json({ message: 'Employee updated successfully', employee });
+    }
   } catch (err) {
-    res.status(500).json({ error: 'Failed to update employee details' });
-  }
-};
-
-const deleteEmployee = async (req, res) => {
-  try {
-    //Write a code here for Deleting all the employees whose salary is greater than 10000
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to delete employees' });
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 module.exports = {
+  getPaginatedEmployees,
+  deleteHighSalaryEmployees,
   createEmployee,
-  getEmployee,
-  updateEmployee,
-  deleteEmployee,
+  getEmployeeById,
+  updateEmployeeById,
 };
